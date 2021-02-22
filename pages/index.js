a6c9a3009986e973e81b77276fb0478e171756f6
mubreda <mubreda@gmail.com>
2021-02-21 22:46:26 -0300

    Portfolio 1.0

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
-import styled from 'styled-components'
+import React from 'react';
+import Footer from '../src/components/commons/Footer';
+import Menu from '../src/components/commons/Menu';
+import Grid from '../src/components/foundation/layout/Grid';
+import Box from '../src/components/foundation/layout/Box';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Capa from '../src/components/commons/Capa';
+import CardsSection from '../src/components/commons/CardsSection';
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Box
+      flex="1"
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <Grid.Container
+        backgroundImage="url(/images/back.jpg)"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="center"
+        backgroundSize="cover"
+      >
+        <Menu />
+        <Capa />
+      </Grid.Container>
+      <CardsSection />
+      <Footer />
+    </Box>
+  );
 }
---
+++ src/components/commons/Card/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import { Text } from '../../foundation/Text';
+import CardWrapper from './styles';
+
+export function Card({
+  image, title, info, url,
+}) {
+  return (
+    <CardWrapper href={url} target="_blank" rel="noopener noreferrer">
+      <CardWrapper.Image
+        src={image}
+        alt="imagem projeto"
+      />
+      <CardWrapper.Footer>
+        <Text
+          variant="subtitle"
+          tag="h2"
+          color="tertiary.main"
+        >
+          {title}
+        </Text>
+        <Text
+          variant="paragraph2"
+          tag="p"
+          color="tertiary.light"
+        >
+          {info}
+        </Text>
+      </CardWrapper.Footer>
+    </CardWrapper>
+  );
+}
+
+export function HLCard({
+  image, title, info, url,
+}) {
+  return (
+    <CardWrapper
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      width="900px"
+      flexDirection="row"
+    >
+      <CardWrapper.Image
+        src={image}
+        alt="Imagem projeto"
+        maxWidth="60%"
+      />
+      <CardWrapper.Footer
+        alignItems="flex-start"
+      >
+        <Text
+          variant="subtitle"
+          tag="h2"
+          color="tertiary.main"
+        >
+          {title}
+        </Text>
+        <Text
+          variant="paragraph2"
+          tag="p"
+          color="tertiary.light"
+        >
+          {info}
+        </Text>
+      </CardWrapper.Footer>
+    </CardWrapper>
+  );
+}
